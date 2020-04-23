6fda9b7d228231ae3820da2fa4c2d2b8e9348d09
Robert Diaz <rad8329@gmail.com>
2020-04-23 10:30:16 -0500

    Scala implementation of the 1295 problem

---
+++ implementation/jvm/src/main/scala/com/rad8329/leetcode/scala/findNumberOfEvenNumbersOfDigits/Solution.scala
@@ -0,0 +1,42 @@
+package com.rad8329.leetcode.scala.findNumberOfEvenNumbersOfDigits
+
+/**
+ * Given an array nums of integers, return how many of them contain an even number of digits.
+ *
+ * Example 1:
+ * Input: nums = [12,345,2,6,7896]
+ * Output: 2
+ *
+ * Explanation:
+ * 12 contains 2 digits (even number of digits).
+ * 345 contains 3 digits (odd number of digits).
+ * 2 contains 1 digit (odd number of digits).
+ * 6 contains 1 digit (odd number of digits).
+ * 7896 contains 4 digits (even number of digits).
+ * Therefore only 12 and 7896 contain an even number of digits.
+ *
+ * Example 2:
+ *
+ * Input: nums = [555,901,482,1771]
+ * Output: 1
+ * Explanation:
+ * Only 1771 contains an even number of digits.
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 500
+ * 1 <= nums[i] <= 10^5
+ *
+ * https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+ *
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Poor perfomance because the recursion, but the intent is to do FP, and the Scala right way
+ *
+ * Runtime: 488 ms
+ * Memory Usage: 50.6 MB
+ */
+object Solution {
+  def findNumbers(numbers: Array[Int]): Int = {
+    numbers.map(number => (Math.log10(number) + 1).toInt).count(number => number % 2 == 0)
+  }
+}
--- implementation/jvm/src/test/java/com/rad8329/leetcode/java/findNumberOfEvenNumbersOfDigits/SolutionTest.java
+++ implementation/jvm/src/test/java/com/rad8329/leetcode/java/findNumberOfEvenNumbersOfDigits/SolutionTest.java
@@ -1,6 +1,7 @@
 package com.rad8329.leetcode.java.findNumberOfEvenNumbersOfDigits;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
@@ -10,9 +11,14 @@ class SolutionTest {
         Solution solution = new Solution();
 
         int[] example1 = {12, 345, 2, 6, 7896};
-        assertEquals(2, solution.findNumbers(example1));
-
         int[] example2 = {555, 901, 482, 1771};
+
+        //Java impl
+        assertEquals(2, solution.findNumbers(example1));
         assertEquals(1, solution.findNumbers(example2));
+
+        //Scala impl
+        assertEquals(2, com.rad8329.leetcode.scala.findNumberOfEvenNumbersOfDigits.Solution.findNumbers(example1));
+        assertEquals(1, com.rad8329.leetcode.scala.findNumberOfEvenNumbersOfDigits.Solution.findNumbers(example2));
     }
 }
